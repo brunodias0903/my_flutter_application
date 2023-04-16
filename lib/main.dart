1c73496afab981c6b6db384748f01f522f8f013e
Bruno Dias <vmns50@gmail.com>
2023-04-16 13:46:56 -0400

    My First Commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:my_flutter_application/login/screens/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LoginScreen(),
+      debugShowCheckedModeBanner: false, //Disable Degub Flag
+    );
+  }
+}
